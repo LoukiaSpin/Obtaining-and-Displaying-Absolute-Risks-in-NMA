26b59ef6dc5d8db640cc5e47d67bacd850a43422
LoukiaSpin <spineli.loukia@mh-hannover.de>
2022-04-19 17:51:29 +0200

    Add data file

--- R/Absolute risks_NMA results.R
+++ R/Absolute risks_NMA results.R
@@ -9,21 +9,13 @@
 
 
 
-## Load libraries
-list_of_packages <- "readxl"
-lapply(list_of_packages, require, character.only = TRUE); rm(list_of_packages) 
-
-
-
 ## Load R functions
-source("./32_R models/obtain.absolute.risks_function.R")
+source("./R/obtain.absolute.risks_function.R")
 
 
 
-## Load datasets
-# Baker
-baker <- as.data.frame(
-  read_excel("./31_Datasets/Baker_Dataset.xlsx", na = "NA", sheet = "Analysis"))
+## Load dataset
+load("./data/baker_published.RData")
 
 
 
@@ -32,7 +24,7 @@ res <- absolute_risk(data = baker,
                      ref = "PBO", 
                      base_risk = 0.34,
                      measure = "OR",
-                     log = FALSE) 
+                     log = FALSE)
 
 
 
@@ -41,21 +33,19 @@ rd <- data.frame(res$`versus PBO`,
                  res$point - 340, 
                  res$lower - 340, 
                  res$upper - 340)
-colnames(rd) <- colnames(res); rd
+colnames(rd) <- colnames(res);
 
 
 
+## Bring all together
+table1 <- data.frame(baker,        # odds ratio (basic parameters) as published
+                     res[, 2:4],   # calculated absolute risks
+                     rd[, 2:4])    # caclulated risk differences
+colnames(table1) <- c("versus PBO", 
+                      "OR", "lower", "upper",
+                      "AR", "lower", "upper",
+                      "RD", "lower", "upper");table1
 
-## Using LABA-comparisons and baseline risk for LABA from above
-# dataset
-data2 <- data.frame(LABA_comp = c("tiotropium", "ICS", "ICS+LABA", "PBO"),
-                    point = c(0.82, 1.01, 0.90, round(1/0.84, 2)),
-                    lower = c(0.72, 0.89, 0.80, round(1/0.92, 2)),
-                    upper = c(0.93, 1.15, 1.01, round(1/0.76, 2)))
 
-# Obtain absolute risks
-absolute_risk(data = data2, 
-              ref = "LABA", 
-              base_risk = 0.302,
-              measure = "OR",
-              log = FALSE)
+## Create Table 1 
+table1[order(table1$OR),]
